da25430cc90512561d5b701e43c063ef007e068c
Alex Betts <alex.betts@gmail.com>
2021-11-24 11:28:01 -0500

    Create an SDL window

--- main.cpp
+++ main.cpp
@@ -1,7 +1,30 @@
+#include <SDL.h>
+
 #include <iostream>
 
 int
 main(int argc, char **argv) {
-    std::cout << "Hello, sdl-metal" << std::endl;
+    SDL_SetHint(SDL_HINT_RENDER_DRIVER, "metal");
+    SDL_InitSubSystem(SDL_INIT_VIDEO);
+    SDL_Window *window = SDL_CreateWindow("SDL Metal", -1, -1, 640, 480, SDL_WINDOW_ALLOW_HIGHDPI);
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_PRESENTVSYNC);
+
+    bool quit = false;
+    SDL_Event e;
+
+    while (!quit) {
+        while (SDL_PollEvent(&e) != 0) {
+            switch (e.type) {
+                case SDL_QUIT: {
+                    quit = true;
+                } break;
+            }
+        }
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
     return 0;
 }
